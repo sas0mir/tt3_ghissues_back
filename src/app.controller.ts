eb460fb33ec45066e38b5a3e715f723decda6885
sas_mir <sas_mir@icloud.com>
2024-10-31 23:57:32 +0100

    001: server test, nest learning

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('helloworld')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('create')
+  create() {
+    console.log('post');
   }
 }
