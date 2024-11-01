106b9094abea0eee0e72e388f47cf4a9327ffab9
sas_mir <sas_mir@icloud.com>
2024-11-01 23:48:48 +0100

    002: nest learning, db controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  BadRequestException,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateDto } from './dto/create.dto';
 
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('helloworld')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get/:id')
+  getHello(@Param('id', ParseIntPipe) id: number) {
+    if (id < 1) {
+      throw new BadRequestException('id must by mare than 0');
+    }
+    return id;
   }
 
+  @UsePipes(new ValidationPipe())
   @Post('create')
-  create() {
+  create(@Body() dto: CreateDto) {
     console.log('post');
+    return dto;
   }
 }
